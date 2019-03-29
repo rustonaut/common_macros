1fd9828863e65fb010bf722485eb0152e130453d
Philipp Korber <philippkorber@gmail.com>
2019-03-29 21:15:06 +0100

    chore: initial commit, blank library

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! This library will contains common macros.
+//!
+//! Mainly following macros:
+//!
+//! - `hash_map!`,
+//! - `hash_set!`,
+//! - `b_tree_map!`,
+//! - `b_tree_set!`,
+//! - `expr_count!`
+//! - `insert!` (generalization of map/set/vec insertion)
+//!
+//! I'm not sure if I will add common alt. assertion macros
+//! (e.g. `assert_ok!`, `assert_err!`, `assert_iter_eq!`).
